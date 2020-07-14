145189b608a8d743d2e94a54036a1f6f91007d1c
rklopfer <russell@klopfer.us>
2020-07-14 18:27:59 -0400

    fixes

--- update_stuff.sh
+++ update_stuff.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 # Regenerate the file
+# Commit
+# Push
 /home/rklopfer/.virtualenvs/covid-testing/bin/python \
 ~/Work/Source/Git/GitHub/covid-testing/plot_data.py \
-	"Contra Costa,CA" "Allegheny,PA" "Clark,OH" "Fairfield,CT" \
+	"Contra Costa,CA" "Allegheny,PA" "Clark,OH" "Fairfield,CT" "CA" "PA" "OH" "CT" "USA" \
 	--metric=cases100k \
 	--window=7 \
-	--out_file=/home/rklopfer/Work/Source/Git/GitHub/riklopfer.github.io/stuff.html
-
-# Push to github
-git -C /home/rklopfer/Work/Source/Git/GitHub/riklopfer.github.io/ \
+	--start="2020-03-8" \
+	--out_file=/home/rklopfer/Work/Source/Git/GitHub/riklopfer.github.io/stuff.html \
+&& git -C /home/rklopfer/Work/Source/Git/GitHub/riklopfer.github.io/ \
 	commit -m "updating stuff" stuff.html \
 && git push
 
