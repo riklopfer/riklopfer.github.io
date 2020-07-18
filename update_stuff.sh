e0aedea3e0c973f61e10a3badee41efbaf9803f6
rklopfer <russell@klopfer.us>
2020-07-18 12:34:45 -0400

    multiple plots

--- update_stuff.sh
+++ update_stuff.sh
@@ -5,9 +5,9 @@
 # Push
 /home/rklopfer/.virtualenvs/covid-testing/bin/python \
 ~/Work/Source/Git/GitHub/covid-testing/plot_data.py \
-	"Contra Costa,CA" "Allegheny,PA" "Clark,OH" "Fairfield,CT" "CA" "PA" "OH" "CT" "USA" \
-	--metric=cases100k \
-	--window=7 \
+	"Contra Costa,CA" "CA" "Allegheny,PA" "PA" "Clark,OH" "OH" "Fairfield,CT" "CT" "USA" \
+	--metrics=cases100k,deaths100k \
+	--windows=14,7,0 \
 	--start="2020-03-8" \
 	--out_file=/home/rklopfer/Work/Source/Git/GitHub/riklopfer.github.io/kovid-dashboard.html \
 && git -C /home/rklopfer/Work/Source/Git/GitHub/riklopfer.github.io/ \
