0e17c8b254fd52b00645078f3d8071269f672d08
rklopfer <russell@klopfer.us>
2020-07-20 15:21:37 -0400

    update metrics

--- update_stuff.sh
+++ update_stuff.sh
@@ -6,8 +6,8 @@
 /home/rklopfer/.virtualenvs/covid-testing/bin/python \
 ~/Work/Source/Git/GitHub/covid-testing/plot_data.py \
 	"Contra Costa,CA" "CA" "Allegheny,PA" "PA" "Clark,OH" "OH" "Fairfield,CT" "CT" "USA" \
-	--metrics=cases100k,deaths100k \
-	--windows=14,7,0 \
+	--metrics=tests100k,positive-test-rate,cases100k,deaths100k,tests \
+	--windows=14,7 \
 	--start="2020-03-8" \
 	--out_file=/home/rklopfer/Work/Source/Git/GitHub/riklopfer.github.io/kovid-dashboard.html \
 && git -C /home/rklopfer/Work/Source/Git/GitHub/riklopfer.github.io/ \
