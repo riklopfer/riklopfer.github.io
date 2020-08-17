5e779624c1e255509554869aa6c9194bbee2194c
Russell Klopfer <russell.klopfer@gmail.com>
2020-08-17 19:46:23 -0400

    no tests

--- update_stuff.sh
+++ update_stuff.sh
@@ -8,7 +8,7 @@ LOC=$(dirname $0)
 $HOME/.virtualenvs/covid-testing/bin/python \
 ../covid-testing/plot_data.py \
 	"Contra Costa,CA" "CA" "Allegheny,PA" "PA" "Clark,OH" "OH" "Fairfield,CT" "CT" "USA" \
-	--metrics=tests100k,positive-test-rate,cases100k,deaths100k,tests \
+	--metrics=tests100k,positive-test-rate,cases100k,deaths100k \
 	--windows=14,7 \
 	--start="2020-03-8" \
 	--out_file=$LOC/kovid-dashboard.html \
