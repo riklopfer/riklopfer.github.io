e0edb149c3b065e54a3cdf426ab33abd8019a1e4
Russell Klopfer <russell.klopfer@gmail.com>
2020-11-06 15:37:25 -0500

    updated stuff

--- update_stuff.sh
+++ update_stuff.sh
@@ -1,13 +1,36 @@
 #!/usr/bin/env bash
 
-LOC=$(dirname $0)
+LOC="$(dirname $0)"
+
+# Make sure we have what we need
+# clone or update testing repo
+REPO_LOC="$LOC/covid-testing"
+if [[ -d "$REPO_LOC" ]]; then
+  echo "Pulling repo $REPO_LOC"
+  git pull -C $REPO_LOC
+else
+  echo "Cloning repo to $REPO_LOC"
+  git clone https://github.com/riklopfer/covid-testing.git $REPO_LOC
+fi
+
+# check for venv 
+VENV_DIR="$LOC/covid-venv"
+if [[ -d  "$VENV_DIR" ]]; then
+  echo "venv $VENV_DIR exists"
+else
+  echo "creating venv $VENV_DIR"
+  python3 -m venv "$VENV_DIR"
+  $VENV_DIR/bin/pip install -U pip
+fi
+
+$VENV_DIR/bin/pip install -r $REPO_LOC/requirements.txt
 
 # Pull
 # Regenerate the file
 # Commit
 # Push
 git -C $LOC pull \
-&& ${LOC}/../covid-testing/plot_data.py \
+&& ${REPO_LOC}/plot_data.py \
 	"Contra Costa,CA" "Allegheny,PA" "Clark,OH" "Fairfield,CT" "USA" \
 	--metrics=cases100k,deaths100k,positive-test-rate,tests100k \
 	--windows=7 \
