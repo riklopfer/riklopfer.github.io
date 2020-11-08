4877b0aba31351d5f6aec82151a707fa2b007cc3
Russell Klopfer <russell.klopfer@gmail.com>
2020-11-07 19:42:11 -0500

    moving stuff around

--- update_stuff.sh
+++ update_stuff.sh
@@ -3,33 +3,33 @@ LOC="$(dirname $0)"
 
 # Make sure we have what we need
 # clone or update testing repo
-REPO_LOC="$LOC/covid-plots"
-if [[ -d "$REPO_LOC" ]]; then
-  echo "Pulling repo $REPO_LOC"
-  git pull --ff-only -C $REPO_LOC
+PLOTS_CODE="$LOC/covid-plots"
+if [[ -d "${PLOTS_CODE}" ]]; then
+  echo "Pulling repo ${PLOTS_CODE}"
+  git -C ${PLOTS_CODE} pull --ff-only 
 else
-  echo "Cloning repo to $REPO_LOC"
-  git clone https://github.com/riklopfer/covid-plots.git $REPO_LOC
+  echo "Cloning repo to ${PLOTS_CODE}"
+  git clone https://github.com/riklopfer/covid-plots.git ${PLOTS_CODE}
 fi
 
 # check for venv 
-VENV_DIR="$LOC/covid-venv"
-if [[ -d  "$VENV_DIR" ]]; then
-  echo "venv $VENV_DIR exists"
+VENV_DIR="$LOC/.venv"
+if [[ -d  "${VENV_DIR}" ]]; then
+  echo "venv ${VENV_DIR} exists"
 else
-  echo "creating venv $VENV_DIR"
-  python3 -m venv "$VENV_DIR"
-  $VENV_DIR/bin/pip install -U pip
+  echo "creating venv ${VENV_DIR}"
+  python3 -m venv "${VENV_DIR}"
+  ${VENV_DIR}/bin/pip install -U pip
 fi
 
-$VENV_DIR/bin/pip install -r $REPO_LOC/requirements.txt
+${VENV_DIR}/bin/pip install -r ${PLOTS_CODE}/requirements.txt
 
 # Pull
 # Regenerate the file
 # Commit
 # Push
 git -C $LOC pull --ff-only \
-&& $VENV_DIR/bin/python ${REPO_LOC}/plot_data.py \
+&& ${VENV_DIR}/bin/python ${PLOTS_CODE}/plot_data.py \
   "Contra Costa,CA" "Allegheny,PA" "Clark,OH" "Fairfield,CT" "USA" \
   --metrics=cases100k,deaths100k,positive-test-rate,tests100k \
   --windows=7 \
